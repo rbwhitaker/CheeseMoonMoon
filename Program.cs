e7f8590c7da704c0a06a172d10e6732bc88e3b51
RB Whitaker <rbwhitaker@outlook.com>
2022-02-26 19:13:12 -0700

    Adding cheese.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@ public interface IMoon
 {
     MoonType Type { get; }
     bool IsStolen { get; }
+
+    /// <summary>
+    /// Returns whether the moon could be reasonably interpreted as being made from cheese
+    /// by the average Brit and his hyper-intelligent dog.
+    /// </summary>
+    bool IsCheese { get; }
 }
 
 public enum MoonType { None, Moon, NotAMoonButASpaceStation }
