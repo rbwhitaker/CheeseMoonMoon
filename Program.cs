bfff9cb6e935b7de126ccd7261edcf1825770a78
RB Whitaker <rbwhitaker@outlook.com>
2022-02-26 19:09:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public interface IMoon
+{
+    MoonType Type { get; }
+    bool IsStolen { get; }
+}
+
+public enum MoonType { None, Moon, NotAMoonButASpaceStation }
